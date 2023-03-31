0da4e997e2bf48d8fc2b6c962b70b327a97069e8
ssknu <ssknu@naver.com>
2023-03-31 10:46:46 +0900

    A#3 본문 푸터 추가

--- Program.cs
+++ Program.cs
@@ -8,6 +8,16 @@
             {
                 Console.WriteLine("헤더입니다.");
             }
+
+            Console.WriteLine("article");
+            {
+                Console.WriteLine("본문입니다.");
+
+            }
+            Console.WriteLine("footer");
+            {
+                Console.WriteLine("푸터입니다.");
+            }
         }
     }
 }
