499890083e9cff13b3657b1e70d7a00d3cf60d2e
ssknu <ssknu@naver.com>
2023-03-31 11:43:18 +0900

    A#5 기사1,2

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@
             Console.WriteLine("article");
             {
                 Console.WriteLine("본문입니다.");
+                Console.WriteLine("기사1");      // A#5
+                Console.WriteLine("기사2");      // A#5
 
             }
             Console.WriteLine("footer");
