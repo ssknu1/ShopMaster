704bedb408b9494978e85baa4495362774c88737
ssknu <ssknu@naver.com>
2023-03-31 10:41:03 +0900

    A#2 헤더추가

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("hether");
+            {
+                Console.WriteLine("헤더입니다.");
+            }
         }
     }
 }
